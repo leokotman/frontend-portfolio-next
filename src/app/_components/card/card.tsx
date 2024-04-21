f06c0c834e4d5b0fa12d334bf87971101eb73817
leokot <leoktmn@yandex.ru>
2024-04-21 14:55:46 +0200

    render contacts

--- src/app/_components/card/card.tsx
+++ src/app/_components/card/card.tsx
@@ -2,16 +2,16 @@ import { ReactNode } from 'react';
 import classes from './card.module.scss';
 
 interface CardParams {
-  heading: string;
+  heading?: string;
   children: ReactNode;
   propsClasses?: string;
 }
 
 export default function Card(params: CardParams) {
-  const { heading, children, propsClasses = '' } = params;
+  const { heading = '', children, propsClasses = '' } = params;
   return (
     <article className={`${classes.card} ${propsClasses}`}>
-      <h1 className="text-xl text-center">{heading}</h1>
+      {heading && <h1 className="text-xl text-center">{heading}</h1>}
       {children}
     </article>
   );
