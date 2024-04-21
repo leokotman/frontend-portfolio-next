ed30792d5e70927704d6e200016a0bd68b4b7854
leokot <leoktmn@yandex.ru>
2024-04-21 19:55:09 +0200

    remove comments

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -51,7 +51,6 @@ export default function Contacts() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1501958.7404380776!2d17.029042918818316!3d42.67167099863321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134c3217242c1fcb%3A0x384c9d866effd346!2sMontenegro!5e0!3m2!1sen!2s!4v1713702557374!5m2!1sen!2s"
             width="600"
             height="450"
-            // style="border: 0"
             allowFullScreen={false}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
