0482c4d993d59a9fb3e1773a2cbe2d97354b82a5
leokot <leoktmn@yandex.ru>
2024-05-29 05:07:04 +0200

    use react-query for getting contacts & caching

--- src/app/_lib/hooks.ts
+++ src/app/_lib/hooks.ts
@@ -1,37 +1,21 @@
 import { db } from '@/firebase/config';
 import { collection, getDocs } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import { isEqual } from 'lodash';
 import { IContactsFromDB } from './types';
+import { useQuery } from '@tanstack/react-query';
 
-export const useContacts = () => {
-  const [contacts, setContacts] = useState<IContactsFromDB[]>([]);
+const getContacts = async () => {
+  const querySnapshot = await getDocs(collection(db, 'contacts'));
+  const data: IContactsFromDB[] = [];
+  querySnapshot.forEach((contact) => {
+    data.push(contact.data() as IContactsFromDB);
+  });
 
-  const getContacts = async () => {
-    const querySnapshot = await getDocs(collection(db, 'contacts'));
-    const data: IContactsFromDB[] = [];
-    querySnapshot.forEach((contact) => {
-      data.push(contact.data() as IContactsFromDB);
-    });
+  return data;
+};
 
-    return data;
-  };
-
-  useEffect(() => {
-    const getContactsFromDB = async () => {
-      const contactsFromDB = await getContacts()
-        .then((response) => response)
-        .then((data) => data);
-
-      if (!isEqual(contactsFromDB, contacts)) {
-        setContacts(contactsFromDB as IContactsFromDB[]);
-      }
-      return contactsFromDB;
-    };
-    if (isEqual(contacts, [])) {
-      getContactsFromDB();
-    }
-  }, [contacts]);
-
-  return { contacts };
+export const useContactsQuery = () => {
+  return useQuery({
+    queryKey: ['contacts'],
+    queryFn: async () => await getContacts(),
+  });
 };
