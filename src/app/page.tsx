28117c91c9ab325b053a2760174767f557d67778
Leo Kotman <71670937+leokotman@users.noreply.github.com>
2024-09-12 10:14:00 +0200

    Feature/11 Start redesign portfolio (#51)
    * introduce main style colors, font
    
    * continue restyle: main page
    
    * finish start of redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,72 @@ import {
 import Card from './_components/card/card';
 import { EXPERIENCES, SKILLS } from './_lib/constants';
 import classes from './page.module.scss';
+import Image from 'next/image';
+import { Chip } from './_components/chip/chip';
 
 export default function Home() {
   return (
     <main className="p-4">
-      <header className="container mx-auto text-center">
-        <h1 className="text-2xl mb-3">Lev Kotman</h1>
-        <h2 className="text-xl mb-6">Front-end developer</h2>
-        <p className="max-w-screen-md container mx-auto text-justify text-lg">
-          Experienced Front-end Developer with over 3 years of professional
-          experience specialising in dynamic interfaces. Skilled in delivering
-          high-quality web applications with clean code for various clients.
-          Started developer&apos;s career path in 2020. Love problem-solving,
-          making projects better.
-        </p>
-      </header>
+      <section className="container mx-auto text-center lg:grid lg:grid-cols-2 lg:grid-flow-col flex flex-wrap">
+        <article>
+          <h1 className="text-2xl mb-3">Lev Kotman</h1>
+          <h2 className="text-xl mb-6">Front-end developer</h2>
+          <p className="max-w-screen-md container mx-auto text-justify text-lg">
+            Experienced Front-end Developer with over 3 years of professional
+            experience specialising in dynamic interfaces.
+            <br /> Skilled in delivering high-quality web applications with
+            clean code for various clients. Started developer&apos;s career path
+            in 2020. Love problem-solving, making projects better.
+          </p>
+        </article>
+        <Image
+          src="/img/main-page-bg.png"
+          alt="programmer-with-laptop-react-html-coding"
+          width={582}
+          height={576}
+          style={{
+            objectFit: 'cover',
+          }}
+          priority
+        />
+      </section>
       <section className="container mx-auto max-w-screen-lg flex flex-wrap justify-around gap-10 py-4">
+        <Card propsClasses="flex flex-wrap justify-between gap-y-6">
+          <h2 className="text-xl text-center w-full">Skills</h2>
+          <div className="flex flex-wrap justify-between gap-20 lg:flex-nowrap">
+            <Card>
+              <ul className="flex flex-col gap-4">
+                <span className="flex gap-4">
+                  <h3 className="text-lg">Hard skills</h3>
+                  <CodeBracketIcon className="h-6 w-6 stroke-indigo-900"></CodeBracketIcon>
+                </span>
+                {SKILLS.hardSkills.map((skill, index) => (
+                  <li key={index} className={classes.point}>
+                    <Chip text={skill} />
+                  </li>
+                ))}
+              </ul>
+            </Card>
+            <div
+              className="h-full w-1.5 rounded-md lg:block hidden"
+              style={{ backgroundColor: 'rgba(133, 62, 222, 0.6)' }}
+            />
+            <Card>
+              <ul className="flex flex-col gap-4">
+                <span className="flex gap-4">
+                  <h3 className="text-lg">Soft skills</h3>
+                  <ChatBubbleLeftRightIcon className="h-6 w-6 stroke-indigo-900"></ChatBubbleLeftRightIcon>
+                </span>
+
+                {SKILLS.softSkills.map((skill, index) => (
+                  <li key={index} className={classes.point}>
+                    <Chip text={skill} />
+                  </li>
+                ))}
+              </ul>
+            </Card>
+          </div>
+        </Card>
         <Card>
           <h2 className="text-xl text-center">Experiences</h2>
           <ul className="flex flex-col gap-4">
@@ -37,36 +87,6 @@ export default function Home() {
             })}
           </ul>
           <div className="flex"></div>
-        </Card>
-        <Card propsClasses="flex flex-wrap justify-between gap-6">
-          <h2 className="text-xl text-center w-full">Skills</h2>
-          <Card>
-            <ul className="flex flex-col gap-4">
-              <span className="flex gap-4">
-                <h3 className="text-lg">Hard skills</h3>
-                <CodeBracketIcon className="h-6 w-6 stroke-indigo-900"></CodeBracketIcon>
-              </span>
-              {SKILLS.hardSkills.map((skill, index) => (
-                <li key={index} className={classes.point}>
-                  <p>{skill}</p>
-                </li>
-              ))}
-            </ul>
-          </Card>
-          <Card>
-            <ul className="flex flex-col gap-4">
-              <span className="flex gap-4">
-                <h3 className="text-lg">Soft skills</h3>
-                <ChatBubbleLeftRightIcon className="h-6 w-6 stroke-indigo-900"></ChatBubbleLeftRightIcon>
-              </span>
-
-              {SKILLS.softSkills.map((skill, index) => (
-                <li key={index} className={classes.point}>
-                  <p>{skill}</p>
-                </li>
-              ))}
-            </ul>
-          </Card>
         </Card>
       </section>
     </main>
