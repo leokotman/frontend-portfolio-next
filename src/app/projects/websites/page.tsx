cf1a7010d9b140e0b85cc63207cc50c787622bca
leokot <leoktmn@yandex.ru>
2024-09-17 20:25:07 +0200

    add projects sorting

--- src/app/projects/websites/page.tsx
+++ src/app/projects/websites/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useMemo } from 'react';
 
 import { useAppContext } from '@/app/_components/context/context';
 import { Loader } from '@/app/_components/loader/loader';
@@ -7,12 +8,21 @@ import ProjectLayout from '@/app/_components/projectLayout/projectLayout';
 const Websites = () => {
   const { projects, isLoadingProjects } = useAppContext();
 
+  const sortedProjects = useMemo(() => {
+    return projects.websites.sort((project1, project2) => {
+      return (
+        Number(project2.imgSrc.split('.')[0]) -
+        Number(project1.imgSrc.split('.')[0])
+      );
+    });
+  }, [projects]);
+
   return (
     <section className="flex flex-col gap-6 items-center">
       {isLoadingProjects ? (
         <Loader />
       ) : (
-        projects.websites.map((project) => {
+        sortedProjects.map((project) => {
           return <ProjectLayout project={project} key={project.imgSrc} />;
         })
       )}
